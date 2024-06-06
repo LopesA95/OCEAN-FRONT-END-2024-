8800d998acca0b45d8358512b1a007fd49712a08
Anderson Lopes <123224432+LopesA95@users.noreply.github.com>
2024-06-06 15:31:42 +0000

    ✨ chore: Update CSS styles and add tags to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ export function App() {
     <>
       <div className='card'>
         <h2>Gekko</h2>
+        <div className='tags'>
+          <p>Iniciador</p>
+          <p>Los Angeles</p>
+          <p>Mosh Pit</p>
+          <p>Wingman</p>
+          <p>Dizzy</p>
+          <p>Thrash</p>
+        </div>
         <img src="https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png" alt="Gekko" />
       </div>
     </>
