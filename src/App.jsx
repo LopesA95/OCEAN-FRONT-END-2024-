f9ad71f618c175603cef87b0b76045cb51673649
Anderson Lopes <123224432+LopesA95@users.noreply.github.com>
2024-06-07 13:10:16 +0000

    ✨ chore: Refactor CSS styles and add tags to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
+import { Card } from './componets/Card/Card'
 
 export function App() {
 
   return (
     <>
-      <div className='card'>
-        <h2>Gekko</h2>
-        <div className='tags'>
-          <p>Iniciador</p>
-          <p>Los Angeles</p>
-          <p>Mosh Pit</p>
-          <p>Wingman</p>
-          <p>Dizzy</p>
-          <p>Thrash</p>
-        </div>
-        <img src="https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png" alt="Gekko" />
-      </div>
+      <Card />
     </>
   )
 }
